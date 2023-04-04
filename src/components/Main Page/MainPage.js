956f1c6c7c50b350bd0628d799cac81563efb973
Jovana <jovanamatic3@gmail.com>
2023-04-04 19:16:11 +0200

    Adding search input js and css

--- src/components/Main Page/MainPage.js
+++ src/components/Main Page/MainPage.js
@@ -1,43 +1,52 @@
-import './MainPage.css';
-import { CardPlaceholder } from '../CardPlaceholder/CardPlaceholder';
-import { Card } from '../Card/Card';
-
+import "./MainPage.css";
+import { CardPlaceholder } from "../CardPlaceholder/CardPlaceholder";
+import { Card } from "../Card/Card";
+import { useState } from "react";
 
 export const MainPage = (props) => {
   const mainPage = props.mainPage;
+  const [search, setSearch] = useState("");
 
   let placeholderArr = [];
   for (let index = 0; index <= 6; index++) {
     placeholderArr.push(index);
   }
-  return (
-    <div className='main'>
 
-      <div className='candidates'>
+  const filtreCandidate = mainPage.filter((item) => {
+    if (search.trim() === "") {
+      return true;
+    } else {
+      if (item.name.toUpperCase().includes(search.trim().toUpperCase())) {
+        return true;
+      }
+      return false;
+    }
+  });
+
+  return (
+    <div className="main">
+      <div className="candidates">
         <h2>Candidates</h2>
-        <input type='text' placeholder='Search...'></input>
+        <input
+          type="text"
+          placeholder="Search..."
+          name="search"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
       </div>
       <div className="candidate-list">
-        {
-          mainPage.length === 0 &&
+        {mainPage.length === 0 &&
           placeholderArr.map((item, index) => {
-            return (<CardPlaceholder key={index} />);
-          })
-        }
-        {
-          mainPage.length > 0 &&
-          mainPage.map((item, index) => {
-            return (
-              
-              <Card key={item.id} item={item} />
-
-            );
-          })
-        }
+            return <CardPlaceholder key={index} />;
+          })}
+        {mainPage.length > 0 &&
+          filtreCandidate.map((item, index) => {
+            return <Card key={item.id} item={item} />;
+          })}
       </div>
-
-
-
     </div>
-  )
-}
+  );
+};
