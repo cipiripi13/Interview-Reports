5d0c2673a539be03aed1aeff9c157f80df867736
aleksandar <aleksandarrakic@yahoo.com>
2023-04-05 17:11:54 +0200

    table task finished

--- src/components/CandidateReports/CandidateReports.js
+++ src/components/CandidateReports/CandidateReports.js
@@ -1,14 +1,16 @@
 import './CandidateReports.css';
 import { useEffect, useState } from 'react';
+import { AiOutlineEye } from "react-icons/ai";
 import { useParams } from 'react-router';
 
 export const CandidateReports = () => {
   const { id } = useParams();
   const [candidate, setCandidate] = useState([]);
+  const [candidateReports, setCandidateReports] = useState([]); //Data for table
 
   const singleCandidateFetch = (id) => {
     const url = 'http://localhost:3333/api/candidates?id=' + id;
-    
+
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
@@ -17,15 +19,29 @@ export const CandidateReports = () => {
         }
       });
   };
+
+  const singleCandidateReportFetch = (id) => {
+    const url = 'http://localhost:3333/api/reports?candidateId=' + id;
+    fetch(url)
+      .then(response => response.json())
+      .then((data => {
+
+        setCandidateReports(data)
+
+      }))
+  };
+
+
   useEffect(() => {
     singleCandidateFetch(id);
-   
+    singleCandidateReportFetch(id);
+
   }, [id]);
 
   const dateString = `${candidate.birthday}`;
   const date = new Date(dateString);
   const formattedDateBirthday = date.toDateString();
-  return(
+  return (
     <div className="candidate-reports">
       <h3>Candidate Reports - Candidate Id No-{id}</h3>
 
@@ -42,6 +58,38 @@ export const CandidateReports = () => {
           </div>
         </div>
       )}
-      </div>
+      {/* display table data */}
+      <h2>Reports</h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Interview Date</th>
+            <th colSpan={2}>Status</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          {
+
+
+            candidateReports.map((item) => {
+
+              const dateString = `${item.interviewDate}`;
+              const date = new Date(dateString);
+              const formattedDate = date.toDateString()
+              return (
+                <tr key={item.id}>
+                  <td> {item.companyName} </td>
+                  <td> {formattedDate} </td>
+                  <td> {item.status} </td>
+                  <td> <span > <AiOutlineEye /> </span> </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
