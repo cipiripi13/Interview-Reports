676e6acd00389dc252956c638b1b5e78c7dc99be
aleksandar <aleksandarrakic@yahoo.com>
2023-04-06 19:35:53 +0200

    Conflicts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router';
 import './App.css';
 import { CandidateReports } from './components/CandidateReports/CandidateReports';
-import { CardPlaceholder } from './components/CardPlaceholder/CardPlaceholder';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { MainPage } from './components/Main Page/MainPage';
-import { CandidateReports } from './components/CandidateReports/CandidateReports';
+
 
 function App() {
   const [mainPage, setMainPage] = useState([]);
-  
+
   const mainFetch = () => {
     const url = 'http://localhost:3333/api/candidates';
     fetch(url)
       .then(response => response.json())
       .then((data => setMainPage(data)))
   }
- 
+
 
   useEffect(() => {
     mainFetch()
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
-import { Link } from 'react-router-dom';
+
 export const Header = () => {
   return (
     <div className="header">
-      <Link to="/" style={{color:'black', textDecoration: 'none'}}><h1>Interview Reports</h1></Link>
+      <Link to="/" style={{ color: 'black', textDecoration: 'none' }}><h1>Interview Reports</h1></Link>
       {/* ubacujemo odmah gotovu komp iz react routera kako bi nas navigirala na pocetnu stranu
       --- potrebno je i gore import-ovati LINK */}
       {/* to ='/' nas navigira na pocetnu stranu--- mainPage */}
