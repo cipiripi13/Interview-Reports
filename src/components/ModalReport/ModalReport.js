7c9338cdfa3be15b7ffe91ca5714288b131931cc
cipiripi13 <krstickatarina1990@gmail.com>
2023-04-06 20:35:11 +0200

    final changes

--- src/components/ModalReport/ModalReport.js
+++ src/components/ModalReport/ModalReport.js
@@ -4,7 +4,6 @@ import './modal.css';
 
 export const ModalReport = (props) => {
 
-  // i ovde nam stize taj closeModal iz komp CandidateReport gde smo na 146 liniji koda prosledili kao props
   const closeModal = props.closeModal;
   const report = props.report;
   console.log(report);
@@ -25,9 +24,9 @@ export const ModalReport = (props) => {
   const formattedDate = date.toDateString()
 
   return (
-    // crni prostor iza modal-a
+
     <div className="modal-overlay">
-      {/* sam modal */}
+
       <div className="modal">
         <h1>{item.candidateName}</h1>
         <div>
@@ -35,41 +34,38 @@ export const ModalReport = (props) => {
             report && (
               <div className='m'>
 
-              
-              <div className='general'>
 
-                <div>
-                  <span>Company:</span>
-                  <h3>{item.companyName}</h3>
+                <div className='general'>
+
+                  <div>
+                    <span>Company:</span>
+                    <h3>{item.companyName}</h3>
+                  </div>
+                  <div>
+                    <span>Interview Date:</span>
+                    <h3>{formattedDate}</h3>
+                  </div>
+                  <div>
+                    <span>Phase</span>
+                    <h3>{item.phase}</h3>
+                  </div>
+                  <div>
+                    <span>Status</span>
+                    <h3>{item.status}</h3>
+                  </div>
+
                 </div>
-                <div>
-                  <span>Interview Date:</span>
-                  <h3>{formattedDate}</h3>
+                <div className='note'>
+                  <span>Note</span>
+                  <p>{item.note}</p>
                 </div>
-                <div>
-                  <span>Phase</span>
-                  <h3>{item.phase}</h3>
-                </div>
-                <div>
-                  <span>Status</span>
-                  <h3>{item.status}</h3>
-                </div>
-                
-              </div>
-              <div className='note'>
-              <span>Note</span>
-              <p>{item.note}</p>
-              </div>
               </div>
             )
           }
         </div>
         <div className="close" onClick={closeModal}>&times;</div>
       </div>
-      {/* close- x - kojim se zatvara modal i stavili smo ga ne da je deo modal-a vec da je deo same crne pozadine */}
-      {/* i da bi modal mogao da zatvori sam sebe a ne ceo ekran dodajemo sledecu f-ju  u candidatReports*/}
-      {/* i ovde dole cekamo taj closeModal iz props-a */}
-      
+
     </div>
 
   )
