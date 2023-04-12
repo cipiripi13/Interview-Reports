1bd3bd4d29da64523a3b742254cb847ee2ac7f27
Jovannaa <116102510+Jovannaa@users.noreply.github.com>
2023-04-12 23:01:25 +0200

    Main Page changes (#17)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,7 @@ import './Header.css';
 export const Header = () => {
   return (
     <div className="header">
-      <h1>Interview Reports</h1>
-      <button className="btn">Candidates</button>
       <Link to="/" style={{ color: 'black', textDecoration: 'none' }}><h1>Interview Reports</h1></Link>
-
       <Link to="/"><button className="btn">Candidates</button></Link>
     </div>
   );
