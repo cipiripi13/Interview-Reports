750fae4603a47c32276fc29bd70977de0117d1bc
aleksandar <aleksandarrakic@yahoo.com>
2023-04-12 22:31:36 +0200

    last try

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,7 @@ export const Header = () => {
   return (
     <div className="header">
       <Link to="/" style={{ color: 'black', textDecoration: 'none' }}><h1>Interview Reports</h1></Link>
-      {/* ubacujemo odmah gotovu komp iz react routera kako bi nas navigirala na pocetnu stranu
-      --- potrebno je i gore import-ovati LINK */}
-      {/* to ='/' nas navigira na pocetnu stranu--- mainPage */}
+
       <Link to="/"><button className="btn">Candidates</button></Link>
     </div>
   );
