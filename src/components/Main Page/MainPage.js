0e0bdff39af1b4f2e0abcff9fe0fd86b2fa7cd27
Jovana <jovanamatic3@gmail.com>
2023-04-12 22:53:32 +0200

    Main Page changes

--- src/components/Main Page/MainPage.js
+++ src/components/Main Page/MainPage.js
@@ -1,19 +1,18 @@
-import "./MainPage.css";
-import { CardPlaceholder } from "../CardPlaceholder/CardPlaceholder";
-import { Card } from "../Card/Card";
-import { useState } from "react";
+import './MainPage.css';
+import { CardPlaceholder } from '../CardPlaceholder/CardPlaceholder';
+import { Card } from '../Card/Card';
+import { useState } from 'react';
+import { CgSearch } from "react-icons/cg"
 
 export const MainPage = (props) => {
   const mainPage = props.mainPage;
-  const [search, setSearch] = useState("");
-
+  const [search, setSearch] = useState('');
   let placeholderArr = [];
   for (let index = 0; index <= 6; index++) {
     placeholderArr.push(index);
   }
-
-  const filtreCandidate = mainPage.filter((item) => {
-    if (search.trim() === "") {
+  const filterCandidate = mainPage.filter((item) => {
+    if (search.trim() === '') {
       return true;
     } else {
       if (item.name.toUpperCase().includes(search.trim().toUpperCase())) {
@@ -22,20 +21,24 @@ export const MainPage = (props) => {
       return false;
     }
   });
-
   return (
     <div className="main">
       <div className="candidates">
         <h2>Candidates</h2>
-        <input
-          type="text"
-          placeholder="Search..."
-          name="search"
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-        />
+        <div className="searchBox">
+          <input className="searchInput"
+            type="text"
+            placeholder="Search..."
+            name="search"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+          <button className="searchButton">
+            <i className="material-icons"><CgSearch /></i>
+          </button>
+        </div>
       </div>
       <div className="candidate-list">
         {mainPage.length === 0 &&
@@ -43,7 +46,7 @@ export const MainPage = (props) => {
             return <CardPlaceholder key={index} />;
           })}
         {mainPage.length > 0 &&
-          filtreCandidate.map((item, index) => {
+          filterCandidate.map((item, index) => {
             return <Card key={item.id} item={item} />;
           })}
       </div>
